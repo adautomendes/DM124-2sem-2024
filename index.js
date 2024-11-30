c33ae277b02ba7842d13a1b2fa9a5c3c7bea66d0
Adauto Mendes <adauto.inatel@gmail.com>
2024-11-30 09:21:52 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,11 @@
+var nome = "Adauto";
+
+console.log("Hello " + nome);
+console.log(`Hello ${nome}`);
+
+function func1() {
+    var escola = "Inatel";
+    console.log(`Escola: ${escola}`);
+}
+
+func1();
